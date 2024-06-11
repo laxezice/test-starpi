b0c5b142732d210f8b64113e1d7bf019c6e6482c
laxezice <45095096+laxezice@users.noreply.github.com>
2024-06-11 11:12:43 +0700

    edit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,22 @@
 module.exports = {
   apps: [
     {
-      name: "starpi",
+      name: "startpi",
       script: "yarn",
       args: "start",
     },
   ],
+
+  deploy: {
+    develop: {
+      user: "ice",
+      host: "159.89.196.226",
+      ref: "origin/main",
+      repo: "https://github.com/laxezice/test-starpi.git",
+      path: "/home/digitalsetup-strapi-user/htdocs/strapi.digitalsetup.co",
+      "pre-deploy-local": "",
+      "post-deploy": "yarn install && pm2 reload ecosystem.config.js",
+      "pre-setup": "",
+    },
+  },
 };
