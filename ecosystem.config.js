06a538362e482ba58d0bf0710c76440ccad9bbf1
laxezice <45095096+laxezice@users.noreply.github.com>
2024-06-11 12:54:30 +0700

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       path: "/home/digitalsetup-strapi-user/htdocs/strapi.digitalsetup.co",
       "pre-deploy-local": "",
       "post-setup": "npm i && npm run build",
-      "post-deploy": "npm i && npm run build && pm2 reload ecosystem.config.js",
+      "post-deploy": "pwd",
       "pre-setup": "",
     },
   },
